b119da6090ea334f3b6f70d14418f6304dc07dc3
Mayibongwe <mayibongwekhumalo1@gmail.com>
2025-04-03 15:48:00 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let ScrollTop =document.createElement('div');
+
+ScrollTop.style.backgroundColor="skyblue";
+ScrollTop.style.color="white";
+ScrollTop.innerText('Top');
+
+document.body.appendChild(screenTop);
