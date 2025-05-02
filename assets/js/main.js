a9ae1ad4d5d8f16c305537b751cbed7867ccbad1
Mayibongwe <mayibongwekhumalo1@gmail.com>
2025-05-02 16:33:06 +0200

    refinements

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,37 +23,3 @@ valueDisplays.forEach((valueDisplay )=>{
 
 });
 
-
-const openBtn = document.querySelector('#open-modal');
-const dialog = document.querySelector('#dialog');
-
-
-openBtn.addEventListener("click", () =>dialog.showModal());
-
-
-
-const  reveal = document.querySelectorAll('.reveal');
-
-const windowHeight = window.innerHeight;
-const revealHeight = reveal[0].getBoundingClientRect().top;
-const revealPoint = 150;
-const revealPoint2 = 150;
-const revealPoint3 = 150;
-const revealPoint4 = 150;
-const revealPoint5 = 150;
-const revealPoint6 = 150;
-const revealPoint7 = 150;
-const revealPoint8 = 150;
-
-
-window.addEventListener('scroll', () => {
-    for (let i = 0; i < reveal.length; i++) {
-        const revealTop = reveal[i].getBoundingClientRect().top;
-        if (revealTop < windowHeight - revealPoint) {
-            reveal[i].classList.add('active');
-        } else {
-            reveal[i].classList.remove('active');
-        }
-    }
-});
-
